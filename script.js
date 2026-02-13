fd6b5817498fd9457b1e0f912e6ab4c8ba5c5e37
Александр Рожков <rendoslegone@MacBook-Air-Aleksandr.local>
2026-02-13 11:05:45 +0300

    added superpuper styles

---
+++ script.js
@@ -0,0 +1,29 @@
+// Fade-in animation
+const elements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+elements.forEach(el => observer.observe(el));
+
+
+// Theme toggle
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+
+    if (document.body.classList.contains('light-theme')) {
+        toggleButton.textContent = "🌑";
+    } else {
+        toggleButton.textContent = "☀";
+    }
+});
+
